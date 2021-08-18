a70c72b0cd53e0169c854b1a6a0d54458516e641
Ryan Skog <ryan.skog@outlook.com>
2021-08-18 15:23:58 -0700

    Desktop header and card section populating dynamic data

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,7 +13,32 @@ $(document).ready(function () {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      for (let i = 0; i < 3; i++) {
+        const element = data[i];
+        $(".card-wrap").append(
+          `<div class="card mb-3" style="width: 100%">
+                <div class="card-body">
+                  <h5 class="card-title">Taco Truck ${i + 1}</h5>
+                  <h6>${element.address}</h6>
+                  <h6>${element.city}, ${element.state} ${
+            element.postal_code
+          }</h6>
+                  <h6 class="open">Open today until 9pm</h6>
+                  <h6 class="phone">
+                    <img src="./assets/images/phone-icon.png" />123-456-7890
+                  </h6>
+                  <div class="d-flex justify-content-between">
+                    <a href="#" class="btn btn-secondary" style="width: 47%"
+                      >Directions</a
+                    >
+                    <a href="#" class="btn btn-secondary" style="width: 47%"
+                      >More Info</a
+                    >
+                  </div>
+                </div>
+              </div>`
+        );
+      }
     });
 
   // DEMO
