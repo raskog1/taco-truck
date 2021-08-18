a046627d7ca66996bc5ccc02e2f82b76731c8ea4
Ryan Skog <ryan.skog@outlook.com>
2021-08-18 14:09:28 -0700

    Initial file structure

---
+++ assets/scripts/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "https://my.api.mockaroo.com/locations.json?key=a45f1200",
+  //     dataType: "json",
+  //   }).success(function (response) {
+  //     // work with response data here
+  //     console.log(response);
+  //   });
+
+  fetch("https://my.api.mockaroo.com/locations.json?key=a45f1200")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
+  // DEMO
+  $(".map").attr(
+    "src",
+    "https://maps.googleapis.com/maps/api/staticmap?center=32.823943,-117.150259&zoom=13&scale=2&size=200x300&maptype=roadmap&format=png&visual_refresh=true&markers=size:small%7Ccolor:0xff0000%7Clabel:1%7C32.823943,-117.150259"
+  );
+});
